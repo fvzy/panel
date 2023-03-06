c6adea4724601b9d9c6cace9c63b0411e0364093
Ditzzy <hello@zyy.sh>
2023-03-06 22:25:11 +0700

    Update wrapper.blade.php

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -6,7 +6,7 @@
         @section('meta')
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+            <meta name="viewport" content="width=1024"> 
             <meta name="csrf-token" content="{{ csrf_token() }}">
             <meta name="robots" content="noindex">
             <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
